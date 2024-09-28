380d8ac911280683d380f4b5d498d8a2381f6aa0
ViktorKobylka <g00425163@atu.ie>
2024-09-28 19:20:04 +0100

    2(b) takes an argument and returns the passed variable

--- lab1.js
+++ lab1.js
@@ -1,2 +1,6 @@
+//2(a)
 const str = () => "Data Representation & Querying"; //return string
 console.log(str());// show result
+//2(b)
+const str2 = a => a; // takes an argument and returns the passed variable
+console.log(str2("Data Representation & Querying")); //show result
